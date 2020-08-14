91c9fa42f9ef99b1a477b9968f5c2fd8f1a19316
vanheaven-ui <vanheaven6@gmail.com>
2020-08-14 18:45:08 +0300

    Add paper clip image for article edit and create views

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
   has_many :tags, through: :taggings
+  has_attached_file :image, styles: { medium: '100vwX100vh>', thumb: '100X100' }
+  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/lpg', 'image/png']
 end
